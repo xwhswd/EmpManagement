aff53b528beec8588672ec7bbc2129d4d98e3fe6
Antonio <xwhswd@163.com>
2023-06-10 16:50:33 +0800

    这是员工管理系统的项目代码

--- src/main/java/com/iweb/arui/pojo/Employee.java
+++ src/main/java/com/iweb/arui/pojo/Employee.java
@@ -1,9 +1,9 @@
-package com.iweb.arui.dao;
+package com.iweb.arui.pojo;
 
 /**
  * @author xwh
  * @version 1.0
  * 2023/6/10
  */
-public interface DeptDao {
+public class Employee {
 }
--- src/main/java/com/iweb/arui/service/DeptService.java
+++
@@ -1,9 +0,0 @@
-package com.iweb.arui.service;
-
-/**
- * @author xwh
- * @version 1.0
- * 2023/6/10
- */
-public interface DeptService {
-}
